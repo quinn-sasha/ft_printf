e1171f34272d24ed1806c45aef437efb366f452b
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-08 16:31:46 +0900

    Added conversion type macro

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,15 +6,29 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 10:34:25 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/08 10:50:23 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/08 15:12:02 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF
 # define FT_PRINTF
 
-# include "libft/libft.h"
+# include "../libft/libft.h"
 # include <stdarg.h>
 
+# define STDOUT 1
+
+# define SINGLE_PERCENT_ERROR 1
+# define LETTER 2
+# define STRING 3
+# define POINTER 4
+# define INTEGER 5
+# define UNSIGNED_INTEGER 6
+# define LOWER_HEX 7
+# define UPPER_HEX 8
+# define PERCENT 9
+# define UNKNOWN_CONVERSION_TYPE 10
+
+int ft_printf(const char *format, ...);
 
 #endif
