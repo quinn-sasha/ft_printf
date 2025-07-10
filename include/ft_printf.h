4959a35be8eff744edca538d81400a3ff6c26e91
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-10 18:39:06 +0900

    Added: unsigned_itoa prototype

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 10:34:25 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/08 15:12:02 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/10 18:31:16 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdarg.h>
 
 # define STDOUT 1
+# define MAX_ADDRESS_LENGTH 16
 
 # define SINGLE_PERCENT_ERROR 1
 # define LETTER 2
@@ -30,5 +31,14 @@
 # define UNKNOWN_CONVERSION_TYPE 10
 
 int ft_printf(const char *format, ...);
+int	ft_vprintf(const char *format, va_list args);
+int print_by_conversion_type(int type, va_list args, char specifier);
+int	determine_conversion_type(char letter);
+int	ft_putchar(char c);
+int	print_specifier(char specifier);
+int	ft_putstr(const char *str);
+int print_address(const void *addr);
+char *unsigned_itoa(unsigned long long num, unsigned int base);
+
 
 #endif
