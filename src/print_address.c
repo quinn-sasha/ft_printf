2fd223956ca6a7384859520eb4281f948c87d2dd
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-11 14:38:51 +0900

    Modified: argument name

--- src/print_address.c
+++ src/print_address.c
@@ -6,19 +6,19 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 15:41:14 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/10 18:55:56 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/11 14:29:17 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int print_address(const void *addr)
+int print_address(const void *address)
 {
 	char *buffer;
 	unsigned long long num;
 	int num_bytes;
 
-	num = (unsigned long long)addr;
+	num = (unsigned long long)address;
 	buffer = unsigned_itoa(num, 16);
 	if (ft_strncmp(buffer, "0", 1) == 0)
 		return ft_putstr("(nil)");
